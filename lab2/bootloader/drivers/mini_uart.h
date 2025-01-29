c7f3f0c866a6d96e674b636c7a142d456246fa85
WlmWu <willwu0916@gmail.com>
2025-01-29 12:36:14 +0800

    feat: move bootloader kernel and implement self-relocation

---
+++ lab2/bootloader/drivers/mini_uart.h
@@ -0,0 +1,18 @@
+#ifndef MINI_UART_H_
+#define MINI_UART_H_
+
+#include "bcm2873/reg_gpio.h"
+#include "bcm2873/reg_uart.h"
+
+void uart_init();
+
+char uart_recv();
+
+char uart_getc();
+unsigned int uart_getu();
+
+void uart_putc(const char c);
+void uart_puts(const char* str);
+
+
+#endif // MINI_UART_H_
