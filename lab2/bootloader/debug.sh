6c04eca62d0edf95e0c83918d83a6cf0892498c2
WlmWu <willwu0916@gmail.com>
2025-02-16 21:46:28 +0800

    refactor: improve bootloader debugging script for dynamic serial port detection

--- lab2/bootloader/debug.sh
+++ lab2/bootloader/debug.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 tmux new-session -d -s debug_session \
-  "tmux set -g mouse on; make run PTY" \; \
-  split-window -v -t debug_session  \; \
-  send-key "picocom -b 115200 /dev/ttys" \; \
+  "tmux set -g mouse on; make run PTY" \;
+echo "Start QEMU..."
+sleep 1
+qemu_output=$(tmux capture-pane -p -t debug_session)
+serial_port=$(echo "$qemu_output" | grep "char device redirected to" | awk '{print $5}')
+tmux split-window -v -t debug_session \
+  "picocom -b 115200 $serial_port" \; \
   attach -t debug_session
