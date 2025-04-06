8024813ff506c12d03a8063e0b7de9c275dcb61c
WlmWu <willwu0916@gmail.com>
2025-04-06 21:31:13 +0800

    feat: add user program to test syscalls

---
+++ lab5/users/user2/src/main.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#include "syscall.h"
+#include "io.h"
+
+#define READ_LEN 64
+
+__attribute__((section(".text.start"))) int main() {
+    asm volatile("mov x27, 0x72");  // Used to track the progress of the program when using gdb 
+
+    int pid = getpid();
+    asm volatile("mov x27, 0x721");
+
+    printf("PID %d\n", pid);
+    asm volatile("mov x27, 0x722");
+
+    puts("hello world !!!!\n");
+    asm volatile("mov x27, 0x723");
+    
+    char buf[READ_LEN];
+    fgets(buf, READ_LEN);
+    puts(buf);
+    asm volatile("mov x27, 0x724");
+
+    // exit();
+    return 0;
+}
